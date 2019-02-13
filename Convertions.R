6f8e52d46ac2ccf8a43d6c83e14b93443d630d61
Anna Nesterovich <ANesterovich@gmail.com>
2019-02-13 15:10:02 -0800

    Final

---
+++ Convertions.R
@@ -0,0 +1,18 @@
+#' Converts numeric temperature data from degrees Fahrenheit to degrees Celsius
+#'
+#' @param fahr the value in Fahrenheit to be converted
+#' @return the converted value in Celsius
+new_fahr_to_celsius <- function(fahr){
+    celsius <- (fahr -32)*5/9
+    return(celsius)
+    
+}
+
+#' Converts numeric temperature data from degrees Celsius to degrees Fahrenheit
+#'
+#' @param celsius the value in Fahrenheit to be converted
+#' @return the converted value in Celsius
+new_celsius_to_fahr <- function(celsius){
+    fahr <- (celsius*9/5) + 32
+    return(fahr)
+}
